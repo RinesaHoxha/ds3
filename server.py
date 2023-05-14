588f1066d58aa92482df8038153b36837b87b78f
RinesaHoxha <122171723+RinesaHoxha@users.noreply.github.com>
2023-05-14 20:40:09 +0200

    Update server.py
    gjenerimi i celesit

--- server.py
+++ server.py
@@ -10,3 +10,14 @@ class Server:
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(2)
         print('Server listening on {}:{}'.format(self.host, self.port))
+        
+    def start(self):
+        client1, address1 = self.server_socket.accept()
+        print('Connection from:', address1)
+        
+        client2, address2 = self.server_socket.accept()
+        print('Connection from:', address2)
+        
+        # Generate RSA key pair
+        self.private_key = RSA.generate(1024)
+        self.public_key = self.private_key.publickey()
