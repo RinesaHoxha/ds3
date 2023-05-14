f10f8202821805af766479a38a0ccc5d49a2f800
RinesaHoxha <122171723+RinesaHoxha@users.noreply.github.com>
2023-05-14 20:37:07 +0200

    Update server.py
    klasa serveri

--- server.py
+++ server.py
@@ -1,1 +1,12 @@
+import socket
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
+class Server:
+    def __init__(self):
+        self.host = socket.gethostname()
+        self.port = 8080
+        self.server_socket = socket.socket()
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(2)
+        print('Server listening on {}:{}'.format(self.host, self.port))
